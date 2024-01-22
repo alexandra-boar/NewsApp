966ca4070b32cc5db1fd1632c02a34b75c19413c
Alexandra Boar <alexandra.boar@fanduel.com>
2024-01-22 10:46:10 +0200

    Refactored persist favorited article flow in Article View Model.

--- NewsApp/ViewModel/ArticleViewModel.swift
+++ NewsApp/ViewModel/ArticleViewModel.swift
@@ -20,14 +20,15 @@ class ArticleViewModel {
         
         articleService.loadArticles { articles, error in
             if let articles {
-                self.articleList = articles
-//                //check if article is in the database
-                for index in articles.indices {
-                    for managedArticle in managedArticles {
-                        if managedArticle.url == articles[index].url {
-                            print("Found match between article \(articles[index])\n and \nmanagedArticle \(managedArticles)")
-                            self.toggleFavoriteForArticle(index: index)
-                        }
+                self.articleList = articles.map { article in
+                    var favoriteArticle = managedArticles.first { managedArticle in
+                        managedArticle.url == article.url
+                    }
+                    favoriteArticle?.isFavorite.toggle()
+                    if let favoriteArticle {
+                        return favoriteArticle
+                    } else {
+                        return article
                     }
                 }
                 completion(nil)
@@ -36,79 +37,80 @@ class ArticleViewModel {
             }
         }
     }
-    
-    func getArticle(index: Int) -> Article? {
-        if let list = articleList {
-            return list[index]
-        } else {
-            return nil
-        }
-    }
-    
-    // cell config
-    func getNumberOfArticles() -> Int {
-        return articleList?.count ?? 0
-    }
-    
-    func toggleFavoriteForArticle(index: Int) {
-        articleList?[index].isFavorite.toggle()
-    }
-    
-    func getArticleTitle(index: Int) -> String? {
-        if let title = getArticle(index: index)?.title {
-            return (title)
-        } else {
-            return (nil)
-        }
-    }
         
-    func isArticleFavorite(index: Int) -> Bool {
-        if let isFavorite = getArticle(index: index)?.isFavorite {
-            return isFavorite
-        } else {
-            return false
+        
+        func getArticle(index: Int) -> Article? {
+            if let list = articleList {
+                return list[index]
+            } else {
+                return nil
+            }
         }
-    }
-
-    
-    func getArticleAuthor(index: Int) -> String? {
-        if let author = getArticle(index: index)?.author {
-            return (author)
-        } else {
-            return ("Unknown author")
+        
+        // cell config
+        func getNumberOfArticles() -> Int {
+            return articleList?.count ?? 0
         }
-    }
-    
-    func getArticleContent(index: Int) -> String? {
-        if let content = getArticle(index: index)?.content {
-            return (content)
-        } else {
-            return ("Could not load content. Read More")
+        
+        func toggleFavoriteForArticle(index: Int) {
+            articleList?[index].isFavorite.toggle()
         }
-    }
-    
-    func getArticleDescription(index: Int) -> String? {
-        if let description = getArticle(index: index)?.description {
-            return (description)
-        } else {
-            return ("Could not load content.")
+        
+        func getArticleTitle(index: Int) -> String? {
+            if let title = getArticle(index: index)?.title {
+                return (title)
+            } else {
+                return (nil)
+            }
         }
-    }
-    
-    func getArticleUrl(index: Int) -> String? {
-        if let url = getArticle(index: index)?.url {
-            return (url)
-        } else {
-            return nil
+        
+        func isArticleFavorite(index: Int) -> Bool {
+            if let isFavorite = getArticle(index: index)?.isFavorite {
+                return isFavorite
+            } else {
+                return false
+            }
         }
-    }
-    
-    func getImageUrl(index: Int) -> String? {
-        if let imageUrl = getArticle(index: index)?.urlToImage {
-            print(imageUrl)
-            return (imageUrl)
-        } else {
-            return nil
+        
+        
+        func getArticleAuthor(index: Int) -> String? {
+            if let author = getArticle(index: index)?.author {
+                return (author)
+            } else {
+                return ("Unknown author")
+            }
         }
-    }
+        
+        func getArticleContent(index: Int) -> String? {
+            if let content = getArticle(index: index)?.content {
+                return (content)
+            } else {
+                return ("Could not load content. Read More")
+            }
+        }
+        
+        func getArticleDescription(index: Int) -> String? {
+            if let description = getArticle(index: index)?.description {
+                return (description)
+            } else {
+                return ("Could not load content.")
+            }
+        }
+        
+        func getArticleUrl(index: Int) -> String? {
+            if let url = getArticle(index: index)?.url {
+                return (url)
+            } else {
+                return nil
+            }
+        }
+        
+        func getImageUrl(index: Int) -> String? {
+            if let imageUrl = getArticle(index: index)?.urlToImage {
+                print(imageUrl)
+                return (imageUrl)
+            } else {
+                return nil
+            }
+        }
 }
